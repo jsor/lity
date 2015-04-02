9589bb90dc52ea3d5b91fe54a83baeb1e2a0bb8e
Jan Sorgalla <jsorgalla@gmail.com>
2015-04-02 09:44:01 +0200

    Prepare release v1.1.1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,11 @@ Changelog
 All notable changes to this project will be documented in this file.
 This project adheres to [Semantic Versioning](http://semver.org).
 
+1.1.1 - 2015-04-02
+------------------
+
+* Fixed: Removed jQuery specific .end() for Zepto compatibility
+
 1.1.0 - 2015-04-02
 ------------------
 
--- dist/lity.js
+++ dist/lity.js
@@ -1,4 +1,4 @@
-/*! Lity - v1.1.0 - 2015-04-02
+/*! Lity - v1.1.1 - 2015-04-02
 * http://sorgalla.com/lity/
 * Copyright (c) 2015 Jan Sorgalla; Licensed MIT */
 (function(window, factory) {
@@ -373,7 +373,7 @@
         return popup.options(options);
     }
 
-    lity.version = '1.1.0';
+    lity.version = '1.1.1';
     lity.handlers = $.proxy(settings, lity, _defaultHandlers);
     lity.options = $.proxy(settings, lity, _defaultOptions);
 
