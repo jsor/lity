d88e95006a4b98e565056b1b4e89c5cca0520901
Jan Sorgalla <jsorgalla@gmail.com>
2015-05-15 16:37:38 +0200

    Prepeare release v1.2.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,12 @@ Changelog
 All notable changes to this project will be documented in this file.
 This project adheres to [Semantic Versioning](http://semver.org).
 
+1.2.0 - 2015-05-15
+------------------
+
+* Changed: The iFrame handler is now the latest handler in the auto-detection
+  chain and can now handle all types of URLs.
+
 1.1.2 - 2015-04-23
 ------------------
 
--- dist/lity.js
+++ dist/lity.js
@@ -1,4 +1,4 @@
-/*! Lity - v1.1.2 - 2015-05-15
+/*! Lity - v1.2.0 - 2015-05-15
 * http://sorgalla.com/lity/
 * Copyright (c) 2015 Jan Sorgalla; Licensed MIT */
 (function(window, factory) {
@@ -363,7 +363,7 @@
         return popup.options(options);
     }
 
-    lity.version = '1.1.2';
+    lity.version = '1.2.0';
     lity.handlers = $.proxy(settings, lity, _defaultHandlers);
     lity.options = $.proxy(settings, lity, _defaultOptions);
 
