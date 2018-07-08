e9e7d3146c385042b9ac7aaa987b576e4e6acebe
RavanH <ravanhagen@gmail.com>
2018-07-08 02:44:03 +0200

    Move all external handlers to plugins

---
+++ src/plugins/youtube/youtube.js
@@ -0,0 +1,30 @@
+(function(window, factory) {
+    if (typeof define === 'function' && define.amd) {
+        define(['lity'], function(lity) {
+            factory(lity);
+        });
+    } else if (typeof module === 'object' && typeof module.exports === 'object') {
+        factory(require('lity'));
+    } else {
+        factory(window.lity);
+    }
+}(typeof window !== "undefined" ? window : this, function(lity) {
+    'use strict';
+
+    var _regex = /(youtube(-nocookie)?\.com|youtu\.be)\/(watch\?v=|v\/|u\/|embed\/?)?([\w-]{11})(.*)?/i;
+
+    lity.handlers('youtube', function(target, instance) {
+        var matches = _regex.exec(target);
+
+        if (!matches) {
+            return false;
+        }
+
+        return lity.iframe(
+			'https://www.youtube' + (matches[2] || '') + '.com/embed/' + matches[4] + '?autoplay=1',
+            instance,
+            matches[5],
+            target
+        );
+    });
+}));
