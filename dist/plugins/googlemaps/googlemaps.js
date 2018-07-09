ec8b16b910f2c75507531e9a3a36fdd677b561de
Rolf Allard van Hagen <ravanhagen@gmail.com>
2018-07-09 08:00:46 +0200

    Move all external handlers to plugins (#184)

---
+++ dist/plugins/googlemaps/googlemaps.js
@@ -0,0 +1,35 @@
+/*! Lity - v3.0.0-dev - 2018-04-20
+* http://sorgalla.com/lity/
+* Copyright (c) 2015-2018 Jan Sorgalla; Licensed MIT */
+(function(window, factory) {
+    if (typeof define === 'function' && define.amd) {
+        define(['lity'], function(lity) {
+            factory(lity);
+        });
+    } else if (typeof module === 'object' && typeof module.exports === 'object') {
+        factory(require('lity'));
+    } else {
+        factory(window.lity);
+    }
+}(typeof window !== "undefined" ? window : this, function(lity) {
+    'use strict';
+
+    var _regex = /((maps|www)\.)?google\.([^\/\?]+)\/?((maps\/?)?\?)(.*)/i;
+
+    lity.handlers('googlemaps', function(target, instance) {
+        var matches = _regex.exec(target);
+
+        if (!matches) {
+            return false;
+        }
+
+        return lity.iframe(
+			'https://www.google.' + matches[3] + '/maps?' + matches[6],
+            instance,
+            {
+                output: matches[6].indexOf('layer=c') > 0 ? 'svembed' : 'embed'
+            },
+            target
+        );
+    });
+}));
---
+++ src/plugins/googlemaps/googlemaps.js
@@ -0,0 +1,32 @@
+(function(window, factory) {
+    if (typeof define === 'function' && define.amd) {
+        define(['lity'], function(lity) {
+            factory(lity);
+        });
+    } else if (typeof module === 'object' && typeof module.exports === 'object') {
+        factory(require('lity'));
+    } else {
+        factory(window.lity);
+    }
+}(typeof window !== "undefined" ? window : this, function(lity) {
+    'use strict';
+
+    var _regex = /((maps|www)\.)?google\.([^\/\?]+)\/?((maps\/?)?\?)(.*)/i;
+
+    lity.handlers('googlemaps', function(target, instance) {
+        var matches = _regex.exec(target);
+
+        if (!matches) {
+            return false;
+        }
+
+        return lity.iframe(
+			'https://www.google.' + matches[3] + '/maps?' + matches[6],
+            instance,
+            {
+                output: matches[6].indexOf('layer=c') > 0 ? 'svembed' : 'embed'
+            },
+            target
+        );
+    });
+}));
